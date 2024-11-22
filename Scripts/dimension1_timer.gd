9ff43004d7b62ca5dd8f1719d18b57ceb84b01a7
Fire Game <kudzejcookc@gmail.com>
2024-11-22 22:08:12 +0100

    player spawn in dimentions

--- Scripts/dimension1_timer.gd
+++ Scripts/dimension1_timer.gd
@@ -2,6 +2,6 @@ extends Timer
 
 func _on_timeout():
 	var player = get_tree().root.get_node("Dimension1/Player")
-	GlobalObjects.player_coords = player.global_position
-	print(GlobalObjects.player_coords)
+	#GlobalObjects.player_coords = player.global_position
+	#print(GlobalObjects.player_coords)
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
